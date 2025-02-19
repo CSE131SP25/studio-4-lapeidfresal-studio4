ba577badb2b7d6b92731790483dda3597e7a7fd5
andreweidelhoch <andreweidelhoch@localhost>
2025-02-19 14:03:00 -0600

    Flag drawing and drawing from file

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,44 @@
 package studio4;
 
-import java.awt.Color;
-
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
-	public static void main(String[] args) {
+
+	public static void main(String[] args){
+		
+		StdDraw.setPenColor(4, 53, 193);
+		StdDraw.filledRectangle(0.5, 0.5, 0.3, 0.2);
+		
+		
+		
+		double[] x1 = {.25, .25, .4};
+		double[] y1 = {.45, .55, .45};
+		
+		StdDraw.setPenColor(133, 14, 5);
+		StdDraw.filledPolygon(x1, y1);
+		
+		double[] x2 = {.75, .75, .6};
+		double[] y2 = {.45, .55, .45};
+		
+		StdDraw.filledPolygon(x2, y2);
+		
+		StdDraw.line(0.5, 0.5, 0.2, 0.7);
+		StdDraw.line(0.5, 0.5, 0.8, 0.7);
+		
+		StdDraw.setPenColor(33, 107, 16);
+		StdDraw.setPenRadius(0.05);
+		
+		StdDraw.line(0.22, 0.37, 0.78, 0.37);
+		
+		StdDraw.setPenColor(255, 215, 0);
+		StdDraw.filledCircle(0.5, 0.475, 0.05);
+		
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.setPenRadius(0.02);
+		StdDraw.rectangle(0.5, 0.5, 0.3, 0.2);
+		
+		StdDraw.show();
+		
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,47 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		
+		
+		if (shape.equals("triangle")) {
+			double x1 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+			
+			StdDraw.line(x1, y1, x2, y2);
+			StdDraw.line(x2, y2, x3, y3);
+			StdDraw.line(x1, y1, x3, y3);
+		}
+		
+		else if (shape.equals("rectangle")){
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+		
+			StdDraw.rectangle(x, y, halfWidth, halfHeight);
+		}
+		
+		else if (shape.equals("ellipse")){
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+		
+			StdDraw.ellipse(x, y, halfWidth, halfHeight);
+			
+		}
+		
+		
+		StdDraw.show();
+		
 	}
 }
